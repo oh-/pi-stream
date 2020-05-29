66fd5c549e5dfcfa70b41a7a1ad89f0a2a14a1e9
Samuel Overington <s.c.overington@gmail.com>
2020-05-29 15:19:28 +0100

    add template items for media and navigation

--- webapp/app.py
+++ webapp/app.py
@@ -2,10 +2,61 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+media_items = [
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 124,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', media_items=media_items)
 
 @app.route('/add')
 def add():
