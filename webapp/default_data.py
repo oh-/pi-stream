b1e6d064a434c3dce409f597459b9bd706ebb37f
Samuel Overington <s.c.overington@gmail.com>
2020-06-05 16:54:37 +0100

    add testing data

---
+++ webapp/default_data.py
@@ -0,0 +1,52 @@
+media_items = [
+    {
+        'title': 'Modern Family',
+        'id': 123,
+        'file_name': 'modernfam.S11E01.mp4',
+        'thumbnail': 'modernfamily.jpg',
+        'type': 'video/mp4',
+        'categories': ['tv-series', 'comedy'],
+        'description': 'Modern Family',
+        'thumbnail_url': 'https://www.citytv.com/wp-content/uploads/2012/11/img-allshows-modern_family-S7.jpg'
+    },
+    {
+        'title': 'Catch Me if you can',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 123,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    },
+    {
+        'title': 'A movie',
+        'id': 124,
+        'file_location': '/Users/samuel/Movies/Catch Me if You Can-b09x4lxs.mp4',
+        'categories': ['film', 'leo'],
+        'description': 'A movie',
+        'thumbnail_url': 'https://fairytalepictures.files.wordpress.com/2016/02/img_5939-1.jpeg'
+    }
+]
